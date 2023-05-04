77480864fd6143f541599117b1ecb44610c9d42d
baraa62 <132255698+baraa62@users.noreply.github.com>
2023-05-04 06:44:51 +0300

    mmm

--- lib/baraa.dart
+++ lib/baraa.dart
@@ -7,6 +7,10 @@ class MyWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar:AppBar(
+        title: Text('baraa'),
+      ),
+    );
   }
 }
